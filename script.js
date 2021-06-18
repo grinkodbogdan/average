1c61f7d5921070d71336c19777d40a84521adbcc
grinkodbogdan <grinkod.bogdan@gmail.com>
2021-06-18 12:07:20 +0200

    darkMode and updated average
    added darkMode;
    excluded zachowanie from average.

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-
+let zachowanieElement;
 
 const calculate = function() {
-  let totalSum = 0;
+  zachowanieElement = document.getElementById('zachowanie');
+  let totalSum = 0 ;
   const inputs = document.getElementsByTagName('select');
   for(const i of inputs) {
     totalSum += parseInt(i.value);
   }
- let inputsCnt = inputs.length;
- let average = (totalSum / inputsCnt);
+ let inputsCnt = inputs.length - 1;
+ let average = ((totalSum - parseInt(zachowanieElement.value)) / inputsCnt);
  let message = '';
  const resultElement = document.getElementById('result');
  if (average === 6) {
    message = ' nie jest mozliwe!';
- } else if (average >= 4.75) {
+ } else if (average >= 4.75 && parseInt(zachowanieElement.value) > 4) {
    message = ' Czerwony pasek!'
- } else if (average < 4.75) {
+ } else if (average < 4.75 || parseInt(zachowanieElement.value) < 5) {
    message = ' Nie dostaniesz w tym roku czerwonego paska!'
  }
 
  resultElement.innerText = average.toFixed(2) + message;
+}
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
